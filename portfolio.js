47fc09cdcf45b3e4af5a79740c346c22cef5d2f1
Gani124 <ps24@pdx.edu>
2023-03-12 01:22:38 -0800

    Portfolio Fourth commit.

---
+++ portfolio.js
@@ -0,0 +1,34 @@
+const submit_btn = document.getElementById("contactme");
+submit_btn.addEventListener("submit", submit);
+
+function submit(eve)
+{
+    eve.preventDefault();
+    let name = document.getElementById("contactname").value;
+    let email = document.getElementById("mailingadd").value;
+    let message = document.getElementById("message").value;
+
+    if (!name && !email && !message)
+    {
+        console.warn("you must enter some data to submit this form");
+    } 
+    else 
+    {
+        if (!name) 
+        {
+          name = "no submission";
+        }
+        if (!email) 
+        {
+          email = "no submission";
+        }
+        if (!message) 
+        {
+          message = "no submission";
+        }
+        console.log("========== Form Submission ==========");
+        console.log("Username: ", name);
+        console.log("Email: ", email);
+        console.log("Message:", message);
+    }
+}
