32c569ea956de574fef18a122d2e78e3b1f9ef61
Gani124 <ps24@pdx.edu>
2023-03-17 14:03:56 -0700

    Portfolio seventh commit.

--- portfolio.js
+++ portfolio.js
@@ -1,35 +1,39 @@
-const submit_btn = document.getElementById("contactme");
-submit_btn.addEventListener("submit", submit);
+var form = document.querySelector("form");
+form.addEventListener("submit",submit)
 
 function submit(eve)
 {
     eve.preventDefault();
-    let name = document.getElementById("contactname").value;
-    let email = document.getElementById("mailingadd").value;
-    let message = document.getElementById("message").value;
+    let name = form.elements[0].value;
+    let email = form.elements[1].value;
+    let message = form.elements[2].value;
 
     if (!name && !email && !message)
     {
         console.warn("you must enter some data to submit this form");
+        alert("please fill the form to proceed further");
     } 
     else 
     {
-        if (!name) 
-        {
-          name = "no submission";
-        }
-        if (!email) 
-        {
-          email = "no submission";
-        }
-        if (!message) 
-        {
-          message = "no submission";
-        }
-        console.log("========== Form Submission ==========");
-        console.log("Username: ", name);
-        console.log("Email: ", email);
-        console.log("Message:", message);
-        alert("your response is recorded");
+      if (!name) 
+      {
+        name = "no submission";
+      }
+      if (!email) 
+      {
+        email = "no submission";
+      }
+      if (!message) 
+      {
+        message = "no submission";
+      }
+      console.log("========== Form Submission ==========");
+      console.log("Username: ", name);
+      console.log("Email: ", email);
+      console.log("Message:", message);
+      alert("your response is recorded");
+      form.elements[0].value = "";
+      form.elements[1].value = "";
+      form.elements[2].value = "";
     }
 }
